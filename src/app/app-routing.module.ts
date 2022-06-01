64d4514d6c31731eb289ff66a66f61c240f0241f
v <v>
2022-06-01 11:56:41 +0300

    some small routing fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{
-  path: '',
-  loadChildren: () => import('./pages/root/root.module').then(m => m.RootModule)
-}];
+const routes: Routes = [
+	{
+		path: 'mf1',
+		loadChildren: () => import('./pages/root/root.module').then(m => m.RootModule),
+	},
+	{ path: '**', redirectTo: 'mf1', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
